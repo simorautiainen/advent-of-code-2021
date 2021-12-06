3b80503427e31502038a92e5a7dd5daf03f58626
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-06 15:19:21 +0200

    day 6 solution under 1 ms

---
+++ day6.py
@@ -0,0 +1,26 @@
+import collections
+import itertools
+import numpy as np
+
+with open("inputs/day6") as data:
+  f = data.readlines()
+  data=np.array(f[0].split(',')).astype(np.int32)
+  
+def get_nth_day_total(data, days):
+
+  counter = collections.Counter(data)
+  extras = collections.Counter()
+  for amount, i in enumerate(itertools.cycle(range(7))):
+    if amount == days:
+      break
+    added_index = (i+2) % 7
+    update_dict = {added_index: counter[i] - extras[i]}
+    extras.update(update_dict)
+    counter.update(update_dict)
+    extras[i] = 0
+
+  return counter.total()
+
+
+print("Part 1", get_nth_day_total(data, 80))
+print("Part 2", get_nth_day_total(data, 256))
