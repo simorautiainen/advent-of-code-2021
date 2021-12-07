4041d0e22143c5075e806ccaad52d58ca85b98f8
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-07 07:19:34 +0200

    day 7 almost got to leaderboards

---
+++ day7.py
@@ -0,0 +1,21 @@
+import collections
+import itertools
+
+with open("inputs/day7") as data:
+  f = data.readlines()
+  data = list(map(int, f[0].split(',')))
+
+
+dicta = collections.defaultdict(int)
+for i in range(min(data), max(data)):
+  for k in data:
+    dicta[i] += abs(k-i)+1
+
+print("Part 1", min(list(dicta.values())))
+
+dicta = collections.defaultdict(int)
+for i in range(min(data), max(data)):
+  for k in data:
+    dicta[i] += sum([d for d in range(abs(k-i)+1)])
+
+print("Part 2", min(list(dicta.values())))
