08c804bb35d4955eb5ce92c2c985eb641ccc6e52
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-08 10:29:24 +0200

    refactoring for better readability

--- day8.py
+++ day8.py
@@ -12,60 +12,50 @@ all_possible = set("abcdefg")
 for i in data:
   actual_numbers = {}
 
-  inp = i[0].strip().split(' ')
+  inp = np.array(i[0].strip().split(' '))
   output = i[1].strip().split(' ')
-  combined = np.array(list(map(lambda x: ''.join(sorted(x)), inp)))
 
   mappings = collections.defaultdict(str)
-  len_arr = np.array([len(k) for k in combined])
+  len_arr = np.array([len(k) for k in inp])
+  actual_numbers[1] = inp[len_arr == 2][0]
+  actual_numbers[4] = inp[len_arr == 4][0]
+  actual_numbers[7] = inp[len_arr == 3][0]
+  actual_numbers[8] = inp[len_arr == 7][0]
 
-  actual_numbers[1] = set(combined[np.where(len_arr == 2)][0])
-  actual_numbers[4] = set(combined[np.where(len_arr == 4)][0])
-  actual_numbers[7] = set(combined[np.where(len_arr == 3)][0])
-  actual_numbers[8] = set(combined[np.where(len_arr == 7)][0])
-
-  six_nine_zero = np.unique(combined[np.where(len_arr==6)])
-  two_three_five = np.unique(combined[np.where(len_arr==5)])
+  six_nine_zero = np.unique(inp[len_arr==6])
+  two_three_five = np.unique(inp[len_arr==5])
 
   for iter in six_nine_zero:
-    is_legit = True
-    for char in actual_numbers[4]:
-      if not char in iter:
-        is_legit = False
-        break
-    if is_legit:
-      actual_numbers[9]=set(iter)
+    if set(actual_numbers[4]) <= set(iter):
+      actual_numbers[9]=iter
       break
-  e = list(actual_numbers[9] ^ all_possible)[0]
 
-  zero_six = list(filter(lambda x: not actual_numbers[9] == set(x), six_nine_zero))
+  e = list(set(actual_numbers[9]) ^ all_possible)[0]
+
+  zero_six = six_nine_zero[~(six_nine_zero == actual_numbers[9])]
 
   for k in zero_six:
-    de = list(actual_numbers[9] ^ set(k))
-    is_legit = True
-    for t in actual_numbers[1]:
-      if t in de:
-        is_legit =False
-    if is_legit:
-      break
+    if set(actual_numbers[1]) <= set(k): # its zero
+      de = list(set(actual_numbers[9]) ^ set(k))
+
   de.remove(e)
   d = de[0]
 
-  actual_numbers[6] = set(list(filter(lambda x: d in x, zero_six))[0])
+  actual_numbers[6] = next(filter(lambda x: d in x, zero_six))
+  actual_numbers[0] = zero_six[zero_six != actual_numbers[6]][0]
+  
+  c = list(set(actual_numbers[6]) ^ all_possible)[0]
 
-  actual_numbers[0] = set(list(filter(lambda x: not set(x) in [actual_numbers[6], actual_numbers[9]], six_nine_zero))[0])
-  c = list(actual_numbers[6] ^ all_possible)[0]
-
-  actual_numbers[2] = set(list(filter(lambda x: e in x, two_three_five))[0])
-  three_five = list(filter(lambda x: not actual_numbers[2] == set(x), two_three_five))
-
-  actual_numbers[3] = set(list(filter(lambda x: c in x, three_five))[0])
-  actual_numbers[5] = set(list(filter(lambda x: not actual_numbers[3] == set(x), three_five))[0])
+  actual_numbers[2] = next(filter(lambda x: e in x, two_three_five))
+  three_five = two_three_five[two_three_five != actual_numbers[2]]
+  actual_numbers[3] = next(filter(lambda x: c in x, three_five))
+  actual_numbers[5] = three_five[three_five != actual_numbers[3]][0]
+  
   final_str = ""
 
   for u in output:
     for key, value in actual_numbers.items():
-      if value == set(u):
+      if set(value) == set(u):
         final_str += str(key)
         
   count += int(final_str)
