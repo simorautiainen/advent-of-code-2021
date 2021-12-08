cb71f38270e5243f13deaca0a71c9a4b21291853
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-08 10:41:36 +0200

    refactored more

--- day8.py
+++ day8.py
@@ -23,29 +23,29 @@ for i in data:
   actual_numbers[8] = inp[len_arr == 7][0]
 
   six_nine_zero = np.unique(inp[len_arr==6])
-  two_three_five = np.unique(inp[len_arr==5])
 
-  for iter in six_nine_zero:
-    if set(actual_numbers[4]) <= set(iter):
-      actual_numbers[9]=iter
+  for number in six_nine_zero:
+    if set(actual_numbers[4]).issubset(set(number)): # number is 9
+      actual_numbers[9]=number
       break
 
-  e = list(set(actual_numbers[9]) ^ all_possible)[0]
+  e = (set(actual_numbers[9]) ^ all_possible).pop()
 
   zero_six = six_nine_zero[six_nine_zero != actual_numbers[9]]
 
-  for k in zero_six:
-    if set(actual_numbers[1]) <= set(k): # its zero
-      de = list(set(actual_numbers[9]) ^ set(k))
+  for number in zero_six:
+    if set(actual_numbers[1]).issubset(set(number)): # number is zero
+      de = set(actual_numbers[9]) ^ set(number)
 
   de.remove(e)
-  d = de[0]
+  d = de.pop()
 
   actual_numbers[6] = next(filter(lambda x: d in x, zero_six))
   actual_numbers[0] = zero_six[zero_six != actual_numbers[6]][0]
   
-  c = list(set(actual_numbers[6]) ^ all_possible)[0]
+  c = (set(actual_numbers[6]) ^ all_possible).pop()
 
+  two_three_five = np.unique(inp[len_arr==5])
   actual_numbers[2] = next(filter(lambda x: e in x, two_three_five))
   three_five = two_three_five[two_three_five != actual_numbers[2]]
   actual_numbers[3] = next(filter(lambda x: c in x, three_five))
@@ -59,7 +59,7 @@ for i in data:
         final_str += str(key)
         
   count += int(final_str)
-  finding_nums = [4,2, 7, 3]
+  finding_nums = [4, 2, 7, 3]
   for u in output:
     if len(u) in finding_nums:
         count1 += 1
