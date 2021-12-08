cceecc175a2aee20b0afccfbb19e656692394354
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-08 09:35:37 +0200

    added and removed newlines

--- day8.py
+++ day8.py
@@ -6,8 +6,8 @@ with open("inputs/day8") as data:
   data = list(map(lambda x: x.split(' | '), f))
 
 count = 0
-
 count1 = 0
+
 all_possible = set("abcdefg")
 for i in data:
   actual_numbers = {}
@@ -23,9 +23,10 @@ for i in data:
   actual_numbers[4] = set(combined[np.where(len_arr == 4)][0])
   actual_numbers[7] = set(combined[np.where(len_arr == 3)][0])
   actual_numbers[8] = set(combined[np.where(len_arr == 7)][0])
-  six_nine_zero = list(set(combined[np.where(len_arr==6)]))
 
+  six_nine_zero = list(set(combined[np.where(len_arr==6)]))
   two_three_five = list(set(combined[np.where(len_arr==5)]))
+
   for iter in six_nine_zero:
     is_legit = True
     for char in actual_numbers[4]:
@@ -37,8 +38,8 @@ for i in data:
       break
   e = list(actual_numbers[9] ^ all_possible)[0]
 
-  a= list(actual_numbers[1] ^ actual_numbers[7])[0]
   zero_six = list(filter(lambda x: not actual_numbers[9] == set(x), six_nine_zero))
+  
   for k in zero_six:
     de = list(actual_numbers[9] ^ set(k))
     is_legit = True
@@ -57,7 +58,6 @@ for i in data:
 
   actual_numbers[2] = set(list(filter(lambda x: e in x, two_three_five))[0])
   three_five = list(filter(lambda x: not actual_numbers[2] == set(x), two_three_five))
-
 
   actual_numbers[3] = set(list(filter(lambda x: c in x, three_five))[0])
   actual_numbers[5] = set(list(filter(lambda x: not actual_numbers[3] == set(x), three_five))[0])
