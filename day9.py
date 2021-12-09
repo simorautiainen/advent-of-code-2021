48fe3fcec6459dc0a54e8ea69869afd2c153516c
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-09 13:17:33 +0200

    refactored

--- day9.py
+++ day9.py
@@ -7,45 +7,40 @@ with open("inputs/day9") as data:
   f = data.readlines()
   data = np.array(list(map(lambda x: list(x.strip()), f))).astype(np.int32)
 
-legits = []
-
 vecs = [point(0,1), point(1,0), point(-1,0), point(0, -1)]
+def is_point_in_bounds(inp_point):
+  return inp_point.x >=0 and inp_point.x < data.shape[1] and inp_point.y >=0 and inp_point.y < data.shape[0]
+
+# part 1
+legits = []
 for iy, ix in np.ndindex(data.shape):
 
-  is_legit = True
+  is_smallest = True
   for vec in vecs:
-    x = ix+vec.x
-    y = iy+vec.y
-    if x >=0 and x < data.shape[1] and y >=0 and y < data.shape[0]:
-      if data[iy, ix] >= data[y, x]:
-        is_legit = False
-  if is_legit:
+    new_p = point(ix+vec.x, iy+vec.y)
+
+    if is_point_in_bounds(new_p) and data[iy, ix] >= data[new_p.y, new_p.x]:
+      is_smallest = False
+      break
+  if is_smallest:
     legits.append(point(ix,iy))
 
 total = 0
 for x,y in legits:
   total += data[y,x]+1
-print(f"total = {total}")
+print(f"Part 1 = {total}")
 
-def find_bas(inp_point, already_checked_locs):
+#part 2
+def find_bas(inp_point: point, legit_locs: list[point]) -> list[point]:
   for vec in vecs:
     new_p = point(inp_point.x+vec.x, inp_point.y+vec.y)
+    
+    # Is new coordinate inbounds of array
+    if (not new_p in legit_locs) and is_point_in_bounds(new_p) and not (data[new_p.y, new_p.x] == 9):
+      legit_locs.append(new_p)
+      find_bas(new_p, legit_locs)
 
-    if new_p in already_checked_locs:
-      continue
-    if (not new_p in already_checked_locs) and new_p.x >=0 and new_p.x < data.shape[1] and new_p.y >=0 and new_p.y < data.shape[0]:
-      legit_point = is_legit_point(new_p, already_checked_locs)
-      if legit_point:
-        already_checked_locs.append(new_p)
-        find_bas(new_p, already_checked_locs)
-
-  return already_checked_locs
-
-
-def is_legit_point(inp_point, already_checked_locs):
-  if data[inp_point.y, inp_point.x] == 9: return False
-  return True
-
+  return legit_locs
 
 sizes = []
 for a_point in legits:
