4a8174fc75655b62a7ead987cf27f23f58b7df49
Simo Rautiainen <simo.rautiainen@gmail.com>
2021-12-14 19:48:06 +0200

    small refactorings and renamings

--- day14.py
+++ day14.py
@@ -1,35 +1,36 @@
 
 import collections
 import functools
+
 with open("inputs/day14") as file:
   f = file.read()
   polymer, pairs = f.split('\n\n')
   pairs = list(map(lambda x: x.split(' -> '), pairs.split('\n')))
 
-def do_x_iter_zipper(polymer, how_many_iters):
+def do_x_iterations(polymer: str, how_many_iters: int) -> collections.Counter:
   zipped = zip(polymer, polymer[1:])
   counter = collections.Counter(polymer)
-  for a_zip in zipped:
-    counter += recurs(a_zip, how_many_iters)
+  for tuple_pair in zipped:
+    counter += recurs(tuple_pair, how_many_iters)
   return counter
 
 @functools.cache
-def recurs(inp_zip, how_many_iters, cur_iter=0):
+def recurs(tuple_pair: tuple, depth: int, current_depth: int = 0) -> collections.Counter:
   counter = collections.Counter()
-  if cur_iter == how_many_iters:
+  if depth == current_depth:
     return counter
-  cur_iter+=1
-  stringi = ''.join(inp_zip)
+  current_depth+=1
+  string_pair: str = ''.join(tuple_pair)
   for pair in pairs:
-    inbetween, inserted = pair
-    if inbetween == stringi:
-      counter[inserted] += 1
-      counter += recurs((inp_zip[0], inserted), how_many_iters, cur_iter)
-      counter += recurs((inserted, inp_zip[1]), how_many_iters, cur_iter)
+    pair_condition, inser_char = pair
+    if pair_condition == string_pair:
+      counter[inser_char] += 1
+      counter += recurs((tuple_pair[0], inser_char), depth, current_depth)
+      counter += recurs((inser_char, tuple_pair[1]), depth, current_depth)
   return counter
 
 
-counter = do_x_iter_zipper(polymer, 10).most_common()
+counter = do_x_iterations(polymer, 10).most_common()
 print("Part 1:", counter[0][1]-counter[-1][1])
-counter = do_x_iter_zipper(polymer, 40).most_common()
+counter = do_x_iterations(polymer, 40).most_common()
 print("Part 2:", counter[0][1]-counter[-1][1])
